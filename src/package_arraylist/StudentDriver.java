276652a281c35d8792df2b8d5b80069c5a4c8fc6
Alfredo <almenaresalfredo@yahoo.com>
2018-09-28 09:20:31 -0400

    Driver class naming changed, and added more comments

--- src/package_arraylist/StudentDriver.java
+++ src/package_arraylist/StudentDriver.java
@@ -21,6 +21,7 @@ class Student{
     String name;
     String[] courses;
 
+    // Constructor initializes Student fields
     public Student(int id, String name, String[] courses) {
         this.id = id;
         this.name = name;
@@ -30,31 +31,31 @@ class Student{
     // Student as a string representation -- id name courses
     @Override
     public String toString(){
-        return String.format("%-2d %-8s %-7s  %-7s  %-7s", id, name, courses[0], courses[1], courses[2]);
+        return String.format("%-3d %-10s %-8s  %-8s  %-8s", id, name, courses[0], courses[1], courses[2]);
     }
     
 }
 
 
-public class CustomerTest {
+public class StudentDriver {
     
     private ArrayList<Student> customers;
 
     // Constructor
-    public CustomerTest() {
+    public StudentDriver() {
         customers = new ArrayList<Student>();    //Initialize list
         
         // Prepare and shuffle ids
-        Integer[] arrIds = new Integer[]{1,2,3,4,5,6,7,8,9,10};
-        ArrayList<Integer> rIds = new ArrayList<>();
-        rIds.addAll(Arrays.asList(arrIds));
+        Integer[] arrIds = new Integer[]{1,2,3,4,5,6,7,8,9,10}; // 10 integer objects to shuffle latter
+        ArrayList<Integer> rIds = new ArrayList<>();         // Declare list of ids
+        rIds.addAll(Arrays.asList(arrIds));                 // Add integers to the list of ids
         
-        Collections.shuffle(rIds); // Shuffle id numbers
+        Collections.shuffle(rIds); // Shuffle list of id numbers
         
         // Prepare and shuffle names 
         String[] arrNames = new String[]{"Lela","Nana","Vicky","Susan","Sarah","Ken","John","Isaiah","Jen","Humbert"};
-        ArrayList<String> rNames = new ArrayList<>();
-        rNames.addAll(Arrays.asList(arrNames));
+        ArrayList<String> rNames = new ArrayList<>();   // List of names to be randomized
+        rNames.addAll(Arrays.asList(arrNames));         // Add names values in the string array to list of names
         
         Collections.shuffle(rNames); // Shuffle numbers
         
@@ -71,11 +72,9 @@ public class CustomerTest {
             new String[]{"ENG101", "MATH122", "COP203"},
             new String[]{"ENG223", "CAP101", "CAP350"}
         };
-        ArrayList<String[]> rCourses = new ArrayList<>();
-        rCourses.addAll(Arrays.asList(arrCourses));
         
         for (int i = 0; i < 10; i++) {
-            customers.add(new Student(rIds.get(i), rNames.get(i), rCourses.get(i)));
+            customers.add(new Student(rIds.get(i), rNames.get(i), arrCourses[i]));
         }
         
         
@@ -85,15 +84,12 @@ public class CustomerTest {
             public int compare(Student o1, Student o2) {
                 return o1.name.compareTo(o2.name);
             }
-        
         });
-        
-        
     }
     
     public void display(){
         // Print to screen, sorted by name
-        System.out.println("Display, sorted by name");
+        System.out.println("Displaying students, sorted by name");
         System.out.println(String.format("%-2s %-8s %s", "Id", "Name", "Courses"));
         for (Student student : customers) {
             System.out.println(student);    //Uses string representation of Student class (toString())
@@ -106,7 +102,7 @@ public class CustomerTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CustomerTest students = new CustomerTest(); // All initialization occurs in constructor 
+        StudentDriver students = new StudentDriver(); // All initialization occurs in constructor 
         students.display();     // Print to screen
         
     }
